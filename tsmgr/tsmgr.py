a4543a70dcabdf121146d08dea3e132179c94265
sam210723 <sam210723@hotmail.com>
2020-03-19 08:21:22 +1000

    Load config file

--- tsmgr/tsmgr.py
+++ tsmgr/tsmgr.py
@@ -5,14 +5,29 @@ https://github.com/sam210723/tsmgr
 MPEG transport stream manager for broadcast monitoring systems
 """
 
+import configparser
 import shutil
 
+config = None
 
 def init():
     print("Starting tsmgr...\n")
 
     # Detect FFmpeg
     detect_deps()
+
+    # Load configuration file
+    load_config()
+
+
+def load_config():
+    """
+    Load configuration from INI file
+    """
+    global config
+
+    config = configparser.ConfigParser()
+    config.read('tsmgr.ini')
 
 
 def detect_deps():
