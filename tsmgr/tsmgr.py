413e6e4a8ffd0f3945a6ec9c10df53beda7dc780
Sam <>
2020-03-19 08:03:11 +1000

    Python setup

---
+++ tsmgr/tsmgr.py
@@ -0,0 +1,16 @@
+"""
+tsmgr.py
+https://github.com/sam210723/tsmgr
+
+MPEG transport stream manager for broadcast monitoring systems
+"""
+
+
+def init():
+    return
+
+try:
+    init()
+except KeyboardInterrupt:
+    print("Exiting...")
+    exit(0)
