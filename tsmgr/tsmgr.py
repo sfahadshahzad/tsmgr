3b5ca73b80cb0ba20b3151aa4bcc427b07719241
sam210723 <sam210723@hotmail.com>
2020-03-19 08:18:14 +1000

    Check for FFmpeg

--- tsmgr/tsmgr.py
+++ tsmgr/tsmgr.py
@@ -5,9 +5,25 @@ https://github.com/sam210723/tsmgr
 MPEG transport stream manager for broadcast monitoring systems
 """
 
+import shutil
+
 
 def init():
-    return
+    print("Starting tsmgr...\n")
+
+    # Detect FFmpeg
+    detect_deps()
+
+
+def detect_deps():
+    """
+    Check for FFmpeg
+    """
+
+    if shutil.which("ffmpeg") is None:
+        print("FFmpeg not found")
+        exit(1)
+
 
 try:
     init()
